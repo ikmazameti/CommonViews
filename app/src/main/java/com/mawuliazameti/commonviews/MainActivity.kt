7453251ca078853c193c2ed93800e940980aaff4
8Bits Technology <60759789+ikmazameti@users.noreply.github.com>
2024-12-21 22:22:28 +0000

    Added interactivity

--- app/src/main/java/com/mawuliazameti/commonviews/MainActivity.kt
+++ app/src/main/java/com/mawuliazameti/commonviews/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.mawuliazameti.commonviews
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.mawuliazameti.commonviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginButton: Button = findViewById(R.id.login)
+
+        loginButton.setOnClickListener {
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            if (username.isEmpty()) {
+                Toast.makeText(this, "Please enter your username!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login successfully!", Toast.LENGTH_SHORT).show()
+
+                binding.loginMessage.text = "Welcome $username"
+                binding.loginMessage.visibility = View.VISIBLE
+            }
+
+            //  Toast.makeText(this, "Login button Clicked", Toast.LENGTH_LONG).show()
         }
+
+
+        binding.rememberMe.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "Remember me enabled", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Remember me disabled", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
     }
 }
